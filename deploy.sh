d35351600bb87fa6eb75b7f887d497fb5acd51c9
Tender9 <1505113506@qq.com>
2024-06-04 11:14:17 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,14 @@
  
 # 忽略错误
 set -e  #有错误抛出错误
+
+git add .
+git commit -m "update"
+git push -f git@github.com:Tender9/blog.git master:main  #提交到这个分支
  
 # 构建
 yarn run build  #然后执行打包命令
- 
+
 # 进入待发布的目录
 cd .vitepress/dist  #进到dist目录
  
