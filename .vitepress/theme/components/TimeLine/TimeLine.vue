405fb2416dd6fbd6d2a8a2df208da094e73d7eff
Tender9 <1505113506@qq.com>
2024-06-24 01:23:29 +0800

    update

--- .vitepress/theme/components/TimeLine/TimeLine.vue
+++ .vitepress/theme/components/TimeLine/TimeLine.vue
@@ -1,61 +1,53 @@
 <!-- @format -->
 
 <template>
-    <Timeline
-        mode="alternate"
-        reverse
-    >
-        <slot></slot>
-        <TimelineItem
-            v-for="item in issues"
-            :key="item.id"
-            :color="item.color"
-        >
-            <span style="color: #a09eb3">{{ item.time }}</span>
+  <Timeline mode="alternate" reverse>
+    <slot></slot>
+    <TimelineItem v-for="item in issues" :key="item.id" :color="item.color">
+      <span style="color: #a09eb3">{{ item.time }}</span>
+      <div>
+        <Card hoverable>
+          <template #cover v-if="item.imgUrl">
             <div>
-                <Card hoverable>
-                    <template
-                        #cover
-                        v-if="item.imgUrl"
-                    >
-                        <div>
-                            <img
-                                v-if="item.imgUrl"
-                                ref="pic"
-                                alt="example"
-                                :src="item.imgUrl"
-                                @click="handlePreview(item)"
-                            />
-                        </div>
-                    </template>
-
-                    <div slot="description">{{ item.description }}</div>
-                </Card>
+              <img
+                v-if="item.imgUrl"
+                ref="pic"
+                alt="example"
+                :src="item.imgUrl"
+                @click="handlePreview(item)"
+              />
             </div>
-        </TimelineItem>
-    </Timeline>
+          </template>
+
+          <h6>{{ item.title }}</h6>
+
+          <div slot="description">{{ item.description }}</div>
+        </Card>
+      </div>
+    </TimelineItem>
+  </Timeline>
 </template>
 
 <script setup>
-    import { ref } from "vue";
-    import "ant-design-vue/dist/reset.css";
-    import { Timeline, TimelineItem, Card } from "ant-design-vue";
-    import issuesList from "./assets/issues.mjs";
+import { ref } from "vue";
+import "ant-design-vue/dist/reset.css";
+import { Timeline, TimelineItem, Card } from "ant-design-vue";
+import issuesList from "./assets/issues.mjs";
 
-    const pic = ref();
-    const issues = ref(issuesList);
+const pic = ref();
+const issues = ref(issuesList);
 
-    const handlePreview = (pic) => {
-        // 图片URL
-        const imageUrl = pic.imgUrl;
+const handlePreview = (pic) => {
+  // 图片URL
+  const imageUrl = pic.imgUrl;
 
-        const imageTitle = pic.description + ".png";
+  const imageTitle = pic.description + ".png";
 
-        // 创建一个新的浏览器窗口或标签页
-        const previewWindow = window.open("about:blank", "_blank");
+  // 创建一个新的浏览器窗口或标签页
+  const previewWindow = window.open("about:blank", "_blank");
 
-        // 在新的页面中写入HTML内容，包括预览图片
-        const htmlContent = `
+  // 在新的页面中写入HTML内容，包括预览图片
+  const htmlContent = `
         <!DOCTYPE html>
         <html lang="en">
         <head>
@@ -69,22 +61,22 @@
         </html>
       `;
 
-        // 将HTML内容写入新页面
-        previewWindow.document.write(htmlContent);
-    };
+  // 将HTML内容写入新页面
+  previewWindow.document.write(htmlContent);
+};
 </script>
 
 <style lang="scss" scoped>
-    :deep() {
-        .ant-card-body {
-            color: var(--vp-c-text-1);
-            background-color: var(--vp-c-bg);
-            text-align: left;
-            padding-left: 1em;
-            font-size: 14px;
-        }
-        .ant-timeline-item-tail {
-            border-color: var(--vp-input-switch-bg-color);
-        }
-    }
+:deep() {
+  .ant-card-body {
+    color: var(--vp-c-text-1);
+    background-color: var(--vp-c-bg);
+    text-align: left;
+    padding-left: 1em;
+    font-size: 14px;
+  }
+  .ant-timeline-item-tail {
+    border-color: var(--vp-input-switch-bg-color);
+  }
+}
 </style>
