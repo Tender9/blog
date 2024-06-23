b27924277ae1134e30e30ea6a79f3795632a4f3b
Tender9 <1505113506@qq.com>
2024-06-23 21:17:27 +0800

    update

--- .vitepress/theme/components/TimeLine/assets/issues.mjs
+++ .vitepress/theme/components/TimeLine/assets/issues.mjs
@@ -5,34 +5,36 @@ import prototype_chain from "./原型链.png";
 import event_loop from "./事件循环.png";
 
 const data = [
-    {
-        time: "2021/02/29",
-        description: "理解原型链",
-        imgUrl: prototype_chain,
-    },
-    {
-        time: "2021/04/06",
-        description: "浏览器渲染：1.构建请求;2.网络传输;3.构建相应;4.网络传输;5.浏览器渲染页面;",
-    },
-    {
-        time: "2021/06/30",
-        description: "事件循环",
-        imgUrl: event_loop,
-    },
-    {
-        time: "2021/06/30",
-        description: "HTTP 状态码",
-        imgUrl: "",
-    },
-    {
-        time: "2024/02/30",
-        description: "React 生命周期 ：https://projects.wojtekmaj.pl/react-lifecycle-methods-diagram/",
-    },
+  {
+    time: "2021/02/29",
+    description: "理解原型链",
+    imgUrl: prototype_chain,
+  },
+  {
+    time: "2021/04/06",
+    description:
+      "",
+  },
+  {
+    time: "2021/06/30",
+    description: "事件循环",
+    imgUrl: event_loop,
+  },
+  {
+    time: "2021/06/30",
+    description: "HTTP 状态码",
+    imgUrl: "",
+  },
+  {
+    time: "2024/02/30",
+    description:
+      "React 生命周期 ：https://projects.wojtekmaj.pl/react-lifecycle-methods-diagram/",
+  },
 ];
 
 data.forEach((item) => {
-    item.id = nanoid();
-    item.color = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  item.id = nanoid();
+  item.color = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 });
 
 export default data;
